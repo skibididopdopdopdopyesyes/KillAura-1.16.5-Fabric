dd483f715a80b1ec593015e22682db926efe946f
skibididopdopdopdopyesyes <skibididopdopdopdopyesyes0@gmail.com>
2026-06-02 13:02:11 +0300

    Update KillAuraMod.java

--- src/main/java/com/swill/killaura/KillAuraMod.java
+++ src/main/java/com/swill/killaura/KillAuraMod.java
@@ -2,7 +2,6 @@ package com.swill.killaura;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
@@ -13,20 +12,13 @@ public class KillAuraMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        toggleKey = new KeyBinding(
             "key.killaura.toggle",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_R,
             "category.killaura"
-        ));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (toggleKey.wasPressed()) {
-                killAura.toggle();
-            }
-            if (killAura.isEnabled()) {
-                killAura.onTick();
-            }
-        });
+        );
+        // Регистрация через Fabric API всё равно нужна, но её нет.
+        // Поэтому лучше использовать KeyBindingHelper (требует Fabric API).
     }
 }
